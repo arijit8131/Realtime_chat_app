5d3ba529466da3913fa8aa7467a1b913d4e6f4a6
arijit8131 <arijitpal.123.a@gmail.com>
2022-07-12 16:47:16 +0530

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 // app.listen(PORT, () => {
 
